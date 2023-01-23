735cd3c38fa76afbb4c7b3562804934d259dd9e0
Nadya <Nadi-lv@yandex.ru>
2023-01-23 15:41:43 +0400

    Выходной

---
+++ Zadacha15/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("Введите  цифру от 1 до 7 = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num<6)
+      Console.WriteLine("не выходной");
+else 
+Console.WriteLine("выходной");
+   
