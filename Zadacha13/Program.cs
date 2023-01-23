90f14707f76296a84b25e56af45586ba2eff132d
Nadya <Nadi-lv@yandex.ru>
2023-01-23 15:41:11 +0400

    Третья цифра

---
+++ Zadacha13/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("Введите  число = ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num<=99 )
+{
+    Console.WriteLine("третьей цифры числа нет");
+    return;
+}
+else 
+{
+    while (num>999) 
+    num = num / 10;
+}    
+Console.WriteLine("третья цифра числа ="+ num%10);
